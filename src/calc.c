d44590f626c7c1b183e27e8a19cfa0008873eb55
Andrey Kalmatskiy <a-k72@mail.ru>
2017-09-05 13:34:33 -0400

    Update calc.c

--- src/calc.c
+++ src/calc.c
@@ -8,12 +8,12 @@
 // Evaluates expression given as a text string.
 // expression: in-out parameter
 //		- As input contains expression having () +-*/ ^ sin cos.
-//		- On syntax error returns the position of this error.
+//		- On syntax error returns the position of this error inside the expression.
 // out_err_msg - out parameter, returns
 //		- an empty string if no syntax error,
 //		- or text of syntax error otherwise.
 // Function returns
-//		- NAN or +-INF or calcullation or syntax errors
+//		- NAN or +-INF on calculation or syntax errors
 //		- expression result otherwise.
 // Sample:
 //	  char buf[100];
